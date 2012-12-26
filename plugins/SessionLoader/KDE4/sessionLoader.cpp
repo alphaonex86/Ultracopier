96ca99e6bd6a76417616cfc7b88bd3c21d5ba595
BRULE Herman <alpha_one_x86@first-world.info>
2012-12-26 07:19:20 +0100

    Fix session loader for KDE4

--- plugins/SessionLoader/KDE4/sessionLoader.cpp
+++ plugins/SessionLoader/KDE4/sessionLoader.cpp
@@ -6,51 +6,52 @@
 
 #include <QFile>
 #include <QDir>
+#include <QCoreApplication>
 
 #include "sessionLoader.h"
-void SessionLoader::setEnabled(bool newValue)
+void SessionLoader::setEnabled(const bool &enabled)
 {
-	ULTRACOPIER_DEBUGCONSOLE(Ultracopier::DebugLevel_Notice,"start, newValue: "+QString::number(newValue));
-	QFile link(QDir::homePath()+"/.kde4/Autostart/ultracopier.sh");
-	if(!newValue)
-	{
-		if(link.exists() && !link.remove())
-			ULTRACOPIER_DEBUGCONSOLE(Ultracopier::DebugLevel_Critical,"unable to remove from the startup: "+link.errorString());
-	}
-	else
-	{
-		if(link.open(QIODevice::WriteOnly))
-		{
-			link.write(QString("#!/bin/bash\n").toLocal8Bit());
-			link.write(QString(QCoreApplication::applicationFilePath()).toLocal8Bit());
-			link.close();
-			if(!link.setPermissions(QFile::ExeOwner|QFile::WriteOwner|QFile::ReadOwner))
-				ULTRACOPIER_DEBUGCONSOLE(Ultracopier::DebugLevel_Critical,"unable to set permissions: "+link.errorString());
-		}
-		else
-			ULTRACOPIER_DEBUGCONSOLE(Ultracopier::DebugLevel_Critical,"unable to open in writing the file: "+link.errorString());
-	}
+    ULTRACOPIER_DEBUGCONSOLE(Ultracopier::DebugLevel_Notice,"start, enabled: "+QString::number(enabled));
+    QFile link(QDir::homePath()+"/.kde4/Autostart/ultracopier.sh");
+    if(!enabled)
+    {
+        if(link.exists() && !link.remove())
+            ULTRACOPIER_DEBUGCONSOLE(Ultracopier::DebugLevel_Critical,"unable to remove from the startup: "+link.errorString());
+    }
+    else
+    {
+        if(link.open(QIODevice::WriteOnly))
+        {
+            link.write(QString("#!/bin/bash\n").toLocal8Bit());
+            link.write(QString(QCoreApplication::applicationFilePath()).toLocal8Bit());
+            link.close();
+            if(!link.setPermissions(QFile::ExeOwner|QFile::WriteOwner|QFile::ReadOwner))
+                ULTRACOPIER_DEBUGCONSOLE(Ultracopier::DebugLevel_Critical,"unable to set permissions: "+link.errorString());
+        }
+        else
+            ULTRACOPIER_DEBUGCONSOLE(Ultracopier::DebugLevel_Critical,"unable to open in writing the file: "+link.errorString());
+    }
 }
 
 bool SessionLoader::getEnabled()
 {
-	ULTRACOPIER_DEBUGCONSOLE(Ultracopier::DebugLevel_Notice,"start, return this value: "+QString::number(QFile::exists(QDir::homePath()+"/.kde4/Autostart/ultracopier.sh")));
-	//return the value into the variable
-	return QFile::exists(QDir::homePath()+"/.kde4/Autostart/ultracopier.sh");
+    ULTRACOPIER_DEBUGCONSOLE(Ultracopier::DebugLevel_Notice,"start, return this value: "+QString::number(QFile::exists(QDir::homePath()+"/.kde4/Autostart/ultracopier.sh")));
+    //return the value into the variable
+    return QFile::exists(QDir::homePath()+"/.kde4/Autostart/ultracopier.sh");
 }
 
-void SessionLoader::setResources(OptionInterface * options,QString writePath,QString pluginPath,bool portableVersion)
+void SessionLoader::setResources(OptionInterface * options,const QString &writePath,const QString &pluginPath,const bool &portableVersion)
 {
-	Q_UNUSED(options);
-	Q_UNUSED(writePath);
-	Q_UNUSED(pluginPath);
-	Q_UNUSED(portableVersion);
+    Q_UNUSED(options);
+    Q_UNUSED(writePath);
+    Q_UNUSED(pluginPath);
+    Q_UNUSED(portableVersion);
 }
 
 /// \brief to get the options widget, NULL if not have
 QWidget * SessionLoader::options()
 {
-	return NULL;
+    return NULL;
 }
 
 /// \brief to reload the translation, because the new language have been loaded
--- plugins/SessionLoader/KDE4/sessionLoader.h
+++ plugins/SessionLoader/KDE4/sessionLoader.h
@@ -13,21 +13,21 @@
 /// \brief Define the session loader
 class SessionLoader : public PluginInterface_SessionLoader
 {
-	Q_OBJECT
-	Q_PLUGIN_METADATA(IID "first-world.info.ultracopier.PluginInterface.SessionLoader/0.4.0.0" FILE "plugin.json")
-	Q_INTERFACES(PluginInterface_SessionLoader)
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "first-world.info.ultracopier.PluginInterface.SessionLoader/0.4.0.0" FILE "plugin.json")
+    Q_INTERFACES(PluginInterface_SessionLoader)
 public:
-	/// \brief to set if it's enabled or not
-	void setEnabled(bool);
-	/// \brief to get if is enabled
-	bool getEnabled();
-	/// \brief set the resources for the plugins
-	void setResources(OptionInterface * options,QString writePath,QString pluginPath,bool portableVersion);
-	/// \brief to get the options widget, NULL if not have
-	QWidget * options();
+    /// \brief to set if it's enabled or not
+    void setEnabled(const bool &enabled);
+    /// \brief to get if is enabled
+    bool getEnabled();
+    /// \brief set the resources for the plugins
+    void setResources(OptionInterface * options,const QString &writePath,const QString &pluginPath,const bool &portableVersion);
+    /// \brief to get the options widget, NULL if not have
+    QWidget * options();
 public slots:
-	/// \brief to reload the translation, because the new language have been loaded
-	void newLanguageLoaded();
+    /// \brief to reload the translation, because the new language have been loaded
+    void newLanguageLoaded();
 };
 
 #endif // SESSION_LOADER_PLUGIN_H
