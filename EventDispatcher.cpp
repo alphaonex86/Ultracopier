367e517143fe35bdacd26ba7b4845feed5bff5d4
BRULE Herman <alpha_one_x86@first-world.info>
2013-01-15 11:38:19 +0100

    Change register to ultracopier to be cleaner

--- EventDispatcher.cpp
+++ EventDispatcher.cpp
@@ -25,6 +25,7 @@ EventDispatcher::EventDispatcher()
 {
     qRegisterMetaType<QList<Ultracopier::ReturnActionOnCopyList> >("QList<Ultracopier::ReturnActionOnCopyList>");
     qRegisterMetaType<QList<Ultracopier::ProgressionItem> >("QList<Ultracopier::ProgressionItem>");
+    qRegisterMetaType<Ultracopier::EngineActionInProgress>("Ultracopier::EngineActionInProgress");
 
     copyServer=new CopyListener(&optionDialog);
     connect(&localListener,&LocalListener::cli,&cliParser,&CliParser::cli,Qt::QueuedConnection);
