05ac06bca0e69f56ef3c3ceae38d8a2cc5ff4fa1
BRULE Herman <alpha_one_x86@first-world.info>
2013-01-24 12:18:38 +0100

    Improve the text

--- OSSpecific.cpp
+++ OSSpecific.cpp
@@ -8,13 +8,13 @@ OSSpecific::OSSpecific(QWidget *parent) :
     ui->setupUi(this);
 #ifndef Q_OS_WIN32
     #if defined(Q_OS_LINUX)
-    ui->label->setText(tr("The replacement of default copy/move system is not supported by the file manager (Dolphin, Nautilus, ...).<br />Ask to the developer to support it."));
+    ui->label->setText(tr("The replacement of default copy/move system is not supported by the file manager (Dolphin, Nautilus, ...).<br />Ask to the developer to support it.<br />You need do the copy/move manually."));
     #elif defined(Q_OS_WIN32)
     ui->label->setText(tr("Don't forget to reboot if previously was installed other similar software<br />(like: Supercopier, Ultracopier in previous version, ...)"));
     #elif defined(Q_OS_MAC)
-    ui->label->setText(tr("The replacement of default copy/move system is not supported and blocked by finder of Mac OS X."));
+    ui->label->setText(tr("The replacement of default copy/move system is not supported and blocked by finder of Mac OS X.<br />You need do the copy/move manually by right clicking on the system tray icon near the clock (not the dock icon)."));
     #else
-    ui->label->setText(tr("The replacement of default copy/move system should be not supported by the file manager.<br />Ask to the developer to support it."));
+    ui->label->setText(tr("The replacement of default copy/move system should be not supported by the file manager.<br />Ask to the developer to support it.<br />You need do the copy/move manually."));
     #endif
 #endif
     if(!QIcon::fromTheme("dialog-warning").isNull())
