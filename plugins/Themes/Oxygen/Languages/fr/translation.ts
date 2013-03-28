5a60e7d950945f536298e1d293fcc3d1b5dea3fb
BRULE Herman <alpha_one_x86@first-world.info>
2013-03-28 17:57:19 +0100

    Fix the text of the themes and do the french translation

--- plugins/Themes/Oxygen/Languages/fr/translation.ts
+++ plugins/Themes/Oxygen/Languages/fr/translation.ts
@@ -4,24 +4,71 @@
 <context>
     <name>Themes</name>
     <message>
-        <location filename="../../interface.cpp" line="+185"/>
+        <location filename="../../interface.cpp" line="+241"/>
         <source>File %1/%2, size: %3/%4</source>
         <translation>Fichier %1/%2, taille: %3/%4</translation>
     </message>
     <message>
-        <location line="+203"/>
+        <location line="+190"/>
         <source>Copy list</source>
         <translation>Liste de copies</translation>
     </message>
     <message>
-        <location line="+5"/>
+        <location line="+2"/>
         <source>Move list</source>
         <translation>Liste de déplacements</translation>
     </message>
     <message>
-        <location line="+364"/>
+        <location line="+421"/>
+        <source>Don&apos;t close if errors are found</source>
+        <translation>Garder ouvert s&apos;il y a des erreurs</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Never close</source>
+        <translation>Ne jamais fermer</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Always close</source>
+        <translation>Toujours fermer</translation>
+    </message>
+    <message>
+        <location line="+2"/>
         <source>File Name, 0KB</source>
         <translation>Nom de fichier, 0Ko</translation>
+    </message>
+    <message>
+        <location line="+183"/>
+        <location line="+12"/>
+        <location line="+12"/>
+        <source>Select a color</source>
+        <translation>Selectionner une coleur</translation>
+    </message>
+</context>
+<context>
+    <name>ThemesFactory</name>
+    <message>
+        <location filename="../../factory.cpp" line="+208"/>
+        <source>Don&apos;t close if errors are found</source>
+        <translation>Garder ouvert s&apos;il y a des erreurs</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Never close</source>
+        <translation>Ne jamais fermer</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Always close</source>
+        <translation>Toujours fermer</translation>
+    </message>
+    <message>
+        <location line="+178"/>
+        <location line="+15"/>
+        <location line="+15"/>
+        <source>Select a color</source>
+        <translation>Selectionner une coleur</translation>
     </message>
 </context>
 <context>
@@ -42,12 +89,7 @@
         <translation>Fermer à la fin des transferts</translation>
     </message>
     <message>
-        <location line="+66"/>
-        <source>0KB/sec</source>
-        <translation>0Ko/s</translation>
-    </message>
-    <message>
-        <location line="+20"/>
+        <location line="+98"/>
         <source>&amp;More</source>
         <translation>Pl&amp;us</translation>
     </message>
@@ -68,22 +110,61 @@
     </message>
     <message>
         <location line="+15"/>
-        <location line="+464"/>
         <source>Limit copy speed at:</source>
         <translation>Limiter la vitesse de copie à:</translation>
     </message>
     <message>
-        <location line="-447"/>
-        <source>Unlimited</source>
-        <translation>Illimité</translation>
-    </message>
-    <message>
-        <location line="+26"/>
+        <location line="+43"/>
         <source>Transfer list</source>
         <translation>Liste de transferts</translation>
     </message>
     <message>
-        <location line="+268"/>
+        <location line="+23"/>
+        <source>Move the selected items to the top</source>
+        <translation>Déplacer l&apos;item selectionné au debut</translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <source>Move up the selected items</source>
+        <translation>Déplacer l&apos;item selectionné vers le haut</translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <source>Move down the selected items</source>
+        <translation>Déplacer l&apos;item selectionné vers le bas</translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <source>Move the selected items to the bottom</source>
+        <translation>Déplacer l&apos;item selectionné à la fin</translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <source>Add file/folder</source>
+        <translation>Ajouter fichier/dossier</translation>
+    </message>
+    <message>
+        <location line="+29"/>
+        <source>Delete the selected items</source>
+        <translation>Supprimer les items sélectionnés</translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <source>Search</source>
+        <translation>Rechercher</translation>
+    </message>
+    <message>
+        <location line="+29"/>
+        <source>Export the transfer list</source>
+        <translation>Exporter la liste de transfert</translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <source>Import the transfer list</source>
+        <translation>Importer la liste de transfert</translation>
+    </message>
+    <message>
+        <location line="+55"/>
         <source>Previous</source>
         <translation>Précédent</translation>
     </message>
@@ -94,72 +175,11 @@
     </message>
     <message>
         <location line="+35"/>
-        <source>Options</source>
-        <translation>Options</translation>
-    </message>
-    <message>
-        <location line="+9"/>
-        <source>Copy end</source>
-        <translation>Fin de copie</translation>
-    </message>
-    <message>
-        <location line="+21"/>
-        <source>At the end of the copy: </source>
-        <translation>À la fin de la copie:</translation>
-    </message>
-    <message>
-        <location line="+39"/>
-        <source>Don&apos;t close if errors are found</source>
-        <translation>Garder ouvert s&apos;il y a des erreurs</translation>
-    </message>
-    <message>
-        <location line="+5"/>
-        <source>Never close</source>
-        <translation>Ne jamais fermer</translation>
-    </message>
-    <message>
-        <location line="+5"/>
-        <source>Always close</source>
-        <translation>Toujours fermer</translation>
-    </message>
-    <message>
-        <location line="+11"/>
-        <source>Speed limit</source>
-        <translation>Limitation de la vitesse</translation>
-    </message>
-    <message>
-        <location line="+31"/>
-        <source> KB/s</source>
-        <translation> Ko/s</translation>
-    </message>
-    <message>
-        <location line="+32"/>
-        <source>File collisions</source>
-        <translation>Collision de fichier</translation>
-    </message>
-    <message>
-        <location line="+21"/>
-        <location line="+60"/>
-        <source>What to do: </source>
-        <translation>Opération à effectuer:</translation>
-    </message>
-    <message>
-        <location line="-21"/>
-        <source>Copy errors</source>
-        <translation>Erreurs de copie</translation>
-    </message>
-    <message>
-        <location line="+74"/>
         <source>Interface</source>
         <translation>Interface</translation>
     </message>
     <message>
-        <location line="+11"/>
-        <source>Show speed as main information</source>
-        <translation>Afficher le réglage de limitation de vitesse de transfert</translation>
-    </message>
-    <message>
-        <location line="+37"/>
+        <location line="+12"/>
         <source>Add file</source>
         <translation>Ajouter un fichier</translation>
     </message>
@@ -188,23 +208,59 @@
         <source>Copy folder</source>
         <translation>Copier un répertoire</translation>
     </message>
-    <message>
-        <location line="-526"/>
-        <source>Ctrl+F</source>
-        <translation>Ctrl+F</translation>
-    </message>
 </context>
 <context>
-    <name>options</name>
+    <name>themesOptions</name>
     <message>
-        <location filename="../../options.ui" line="+18"/>
+        <location filename="../../themesOptions.ui" line="+20"/>
+        <source>Show dual progression</source>
+        <translation>Afficher une double progression</translation>
+    </message>
+    <message>
+        <location line="+20"/>
         <source>Show speed as main information</source>
-        <translation>Montrer la vitesse comme une information principale</translation>
+        <translation>Afficher la vitesse comme information principale</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Start with the &quot;more button&quot; pushed</source>
+        <translation>Déplier automatiquement les détails</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>At the end of the copy: </source>
+        <translation>À la fin de la copie:</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <location line="+59"/>
+        <source>Limit copy speed at:</source>
+        <translation>Limiter la vitesse de copie à:</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source> KB/s</source>
+        <translation> Ko/s</translation>
     </message>
     <message>
         <location line="+13"/>
-        <source>Start with the &quot;more button&quot; pushed</source>
-        <translation>Déplier automatiquement les détails</translation>
+        <source>Speed with progress bar</source>
+        <translation>Vitesse avec bar de progression</translation>
+    </message>
+    <message>
+        <location line="+107"/>
+        <source>Progression color</source>
+        <translation>Couleur de progression</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Show progression in the title</source>
+        <translation>Afficher la progression dans le titre</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Always on top</source>
+        <translation>Toujours au 1er plan</translation>
     </message>
 </context>
 </TS>
