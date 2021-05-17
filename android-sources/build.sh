5a678849400df1dd66df9946d867377943895de2
BRULE Herman <alpha_one_x86@first-world.info>
2021-05-17 19:03:14 -0400

    [Improvement] Compile with 16 cores

--- android-sources/build.sh
+++ android-sources/build.sh
@@ -2,6 +2,6 @@
 cd /home/user/src/
 mkdir ~/build && cd ~/build
 qmake -r ~/src/other-pro/ultracopier-little.pro ANDROID_EXTRA_LIBS+=$ANDROID_DEV/lib/libcrypto.so ANDROID_EXTRA_LIBS+=$ANDROID_DEV/lib/libssl.so
-make -j5
+make -j16
 make install INSTALL_ROOT=/home/user/build/dist/
 androiddeployqt --input android-libultracopier.so-deployment-settings.json --output dist/ --android-platform 28 --deployment bundled --gradle --release
